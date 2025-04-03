95344cc34dec587ed859b181ab8bfa4ad027be4a
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-03 20:45:18 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7656369802:AAGdlo88cewouuiviq-eHoRHdxj_Ktji3To")
+API_ID = int(os.getenv("API_ID", "28795512"))
+API_HASH = os.getenv("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
