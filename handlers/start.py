5fb189dc9c9c0aaa8f6367277113c036f0f09337
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-03 20:54:43 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,13 @@ from aiogram.filters import Command
 
 router = Router()
 
+#@router.message(Command("start"))
+#async def start_command(message: types.Message):
+#    await message.reply("🚀 *Telegram Mass Reporting Bot Running!*\nUse `/addsession {session_string}` to add accounts.")
+
 @router.message(Command("start"))
-async def start_command(message: types.Message):
-    await message.reply("🚀 *Telegram Mass Reporting Bot Running!*\nUse `/addsession {session_string}` to add accounts.")
+async def start_command(message: Message):
+    await message.reply(
+        "🚀 *Telegram Mass Reporting Bot Running\\!* \n"
+        "Use `/addsession {session\\_string}` to add accounts."
+    )
